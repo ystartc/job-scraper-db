55f868eb134918dedc48163fe716bf7d573acd5e
ystartc <startceva.dev@gmail.com>
2023-08-03 12:57:24 -0700

    Created a Job table and established relationship to Data table as many to one.

--- app/models/data.py
+++ app/models/data.py
@@ -1,9 +1,12 @@
+from sqlalchemy import func
 from app import db
 
 class Data (db.Model):
     __tablename__ = 'data'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     html = db.Column(db.Text)
-    browser = db.Column(db.String)
-    type = db.Column(db.String)
+    user_agent = db.Column(db.String)
+    status = db.Column(db.String)
+    url = db.Column(db.String)
+    fetch_date = db.Column(db.Date, default=func.now())
     jobs = db.relationship('Job', back_populates='data', lazy=True)
---
+++ app/models/job.py
@@ -0,0 +1,14 @@
+from sqlalchemy import func
+from app import db
+
+class Job (db.Model):
+    __tablename__ = 'jobs'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    fetch_date = db.Column(db.Date, default=func.now())
+    company = db.Column(db.String)
+    location = db.Column(db.String)
+    about = db.Column(db.Text)
+    salary = db.Column(db.String, nullable=True)
+    data_id = db.Column(db.Integer, db.ForeignKey('data.id'))
+    data = db.relationship('Data', back_populates='jobs')
