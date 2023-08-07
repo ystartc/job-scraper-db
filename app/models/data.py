cd01de10f72df449b528d814b9684ff701d998cf
ystartc <startceva.dev@gmail.com>
2023-08-06 17:59:31 -0700

    Removed user_agent column from Data, removed fetch_data column from Jobs.

--- app/models/data.py
+++ app/models/data.py
@@ -5,7 +5,6 @@ class Data (db.Model):
     __tablename__ = 'data'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     html = db.Column(db.Text)
-    # user_agent = db.Column(db.String)
     status = db.Column(db.String)
     url = db.Column(db.String)
     fetch_date = db.Column(db.Date, default=func.now())
