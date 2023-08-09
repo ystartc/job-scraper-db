6185a0b150c64de9f0e76a9a91555e1234725c6a
ystartc <startceva.dev@gmail.com>
2023-08-09 16:03:33 -0700

    Update procfile

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,8 @@ def create_app():
     app.logger.info("Initializing db with flask app")
     #logging.info("Initializing db with the flask app")
     db.init_app(app)
+    app.logger("Dropping all tables")
+    db.drop_all()
     app.logger.info("Migrating db")
     #logging.info("Migrating db")
     migrate.init_app(app, db)
