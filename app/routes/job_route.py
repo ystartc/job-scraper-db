7e810420d91902a27b5944e6c13391b8f5ec6c2e
ystartc <startceva.dev@gmail.com>
2023-08-11 16:58:32 -0700

    .

--- app/routes/job_route.py
+++ app/routes/job_route.py
@@ -17,7 +17,7 @@ def create_job():
     new_entry = Job.from_dict(request_body)
     
     db.session.add(new_entry)
-    db.session.commit()
+    db.session.commit()fksdjflks
     
     return new_entry.to_dict(), 201
 
