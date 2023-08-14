d8ea5719913432329ff118d452908ea3f7357ce4
ystartc <startceva.dev@gmail.com>
2023-08-14 02:52:39 -0700

    Added more fqueries for job get request.

--- app/routes/job_route.py
+++ app/routes/job_route.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from datetime import datetime, timedelta
 from app.models.job import Job
 from . import valid
 from app import db
@@ -23,12 +24,43 @@ def create_job():
 
 @jobs_bp.route('', methods=['GET'])
 def get_jobs():
-    title_query = request.args.get('title')
     
+    title_query = request.args.get('title')
+    location_query = request.args.get('location')
+    company_query = request.args.get('company')
+    
+    days_ago = request.args.get('days_ago', type=int)
+    posted_since = datetime.utcnow() - timedelta(days=days_ago)
+    # Query the database for jobs posted since the specified number of days ago
+    # jobs = Job.query.filter(Job.posted_date >= posted_since).all()
+
     if title_query:
-        jobs = Job.query.filter(Job.title.ilike('%'+title_query.strip()+'%'))
+        jobs = Job.query.filter(Job.title.ilike('%'+title_query.strip()+'%')).order_by(Job.data.fetch_date.desc())
+    elif title_query and location_query:
+        jobs = Job.query.filter(Job.title.ilike('%'+title_query.strip()+'%'), 
+                                Job.location.ilike('%'+location_query.strip()+'%')
+                                ).order_by(Job.data.fetch_date.desc())
+    elif title_query and location_query and posted_since:
+        jobs = Job.query.filter(Job.title.ilike('%'+title_query.strip()+'%'), 
+                                Job.location.ilike('%'+location_query.strip()+'%'),
+                                Job.data.fetch_date >= posted_since
+                                ).order_by(Job.data.fetch_date.desc())
+    if company_query:
+        jobs = Job.query.filter(Job.company.ilike('%'+company_query.strip()+'%')).order_by(Job.data.fetch_date.desc())
+    elif company_query and location_query:
+        jobs = Job.query.filter(Job.company.ilike('%'+company_query.strip()+'%'), 
+                                Job.location.ilike('%'+location_query.strip()+'%')
+                                ).order_by(Job.data.fetch_date.desc())
+    elif company_query and location_query and posted_since:
+        jobs = Job.query.filter(Job.company.ilike('%'+company_query.strip()+'%'), 
+                                Job.location.ilike('%'+location_query.strip()+'%'),
+                                Job.data.fetch_date >= posted_since
+                                # Job.data.fetch_date.ilike('%'+fetched.strip()+'%')
+                                ).order_by(Job.data.fetch_date.desc())
+    elif posted_since:
+        Job.query.filter(Job.posted_date >= posted_since).order_by(Job.data.fetch_date.desc()).all()
     else:
-        jobs = Job.query.all()
+        jobs = Job.query.all().order_by(Job.data.fetch_date.desc())
     
     return jsonify([entry.to_dict() for entry in jobs]), 200
 
